23d095637c6e92caaba8e31914306d6344022091
Santeri Tuomisto <tuomisto.santeri.m@gmail.com>
2020-10-07 17:03:06 +0300

    http

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,9 +3,14 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!loading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">Empty</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,10 +26,44 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch(
+        'https://vue-section-12-http-demo.firebaseio.com/surveys.json'
+      ).then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        this.isLoading = false;
+        const results = [];
+        for (const id in data) {
+          results.push({ id: id, name: data[id].name, rating: data[id].rating });
+        }
+        this.results = results;
+      }).catch((error) => {
+        this.isLoading = false;
+        console.log(error);
+        this.error = 'Error';
+      });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  }
 };
 </script>
 
